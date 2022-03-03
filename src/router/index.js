68f737ee443dae5d1d510577d885fa648f5e0725
Maria Jose Mendez <majomendez88mena@gmail.com>
2022-03-03 10:41:59 -0600

    lesson 4
    Lesson 4 done: Vue router essentials

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventsView from "../views/EventsView.vue";
+import AboutViewVue from '../views/AboutView.vue';
 
 const router = createRouter({
     history: createWebHistory(
@@ -17,11 +18,7 @@ const router = createRouter({
         {
             path: "/about",
             name: "about",
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import ("../views/AboutView.vue"),
+            component: AboutViewVue
         },
     ],
 });
