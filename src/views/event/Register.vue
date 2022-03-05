52b536712dc360a0c6f852ef3d69577983bd685b
Maria Jose Mendez <majomendez88mena@gmail.com>
2022-03-05 11:18:50 -0600

    TVR: Lesson 6
    TVR: Lesson 6 Programmatic Navigation done

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -3,11 +3,42 @@
 <script>
 export default {
   props: ['eventData'],
+  methods: {
+    registerEvent() {
+      // * Call to API to register for event
+      // * If successful, redirect to event details page
+      this.$router.push({
+        name: 'EventDetails',
+      })
+      //* $router.replace -> redirect to new route, but don't add to history
+      //* $router.push -> add to history
+      //* $router.go -> programmatically invoke the browser back and forward buttons 
+      //* .go(1) -> go forward one page
+      //* .go(-1) -> go back one page       
+    }
+  }
 }
 </script>
 
 <template>
   <div v-if="eventData" >
     <p>Register for the event here</p>
+    <button class="button-style" @click="registerEvent">Register me</button>
   </div>
 </template>
+
+<style scoped>
+.button-style{
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 26px;
+  font-weight: bold;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
