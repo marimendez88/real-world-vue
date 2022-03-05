c744f2176f374dcf7c74bfa0f9a631139f022f8d
Maria Jose Mendez <majomendez88mena@gmail.com>
2022-03-05 16:01:38 -0600

    TVR: Lesson 9
    TVR: Lesson 9 -> In Component route guard in progress

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -3,6 +3,17 @@
 <script>
 export default {
   props: ['eventData'],
+  data(){
+    return {
+      unsavedChanges: true
+    }
+  },
+  beforeRouteLeave() {
+    console.log('onBeforeRouteLeave', this.unsavedChanges)
+    if (this.unsavedChanges) {
+      if (!confirm('You have unsaved changes. Are you sure you want to leave?')) return false;
+    }
+  }
 }
 </script>
 
